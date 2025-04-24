39993a2bd42ef8d3f1ac841bd6334b9f55349a93
Matias <Matibravoneron@gmail.com>
2025-04-24 12:11:44 -0300

    improve design

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.onload = () => {     //Objeto y atributo de la venta = Funcion anonima
+window.onload = () => {   
     document.getElementsByTagName('input')[0].focus() //Elementos por nombre de la etiqueta (segun input)
 }
 
@@ -6,40 +6,41 @@ function addRow() {
     var tbody = document.getElementsByTagName("tbody")[0]
     // Obtener todos los inputs que están en el <tfoot>
     var inputs = document.getElementsByTagName("tfoot")[0].getElementsByTagName("input");
-    var row = tbody.insertRow() //Insertar una nueva fila al final del <tbody>
+    var row = tbody.insertRow()
     var rowIndex = tbody.rows.length - 1;
     row.setAttribute("id", "row" + rowIndex);
     //Recorrer el número de celdas de la primera fila (<tr>) de la tabla
     for (var i = 0; i < 3; i++) {
-        //Crear cada celda dentro de la nueva fila
-        var cell = row.insertCell()
+        var cell = row.insertCell() //Crear cada celda dentro de la nueva fila
         cell.setAttribute("id", "cell" + i)
         cell.innerHTML = inputs[i].value || "N/A";
     }
     // Crear la celda para el botón de eliminar
-    var cell = row.insertCell(); // Cuarta celda (índice 3)
+    var cell = row.insertCell();
     cell.setAttribute("id", "cell3");
     //En la tercera celda (i === 3), insertar un botón "X"
-    var button = document.createElement("button")
-    button.innerHTML = "X"
+    var button = document.createElement("button");
+    button.innerHTML = "X";
+    button.classList.add("btn");
+    button.classList.add("btn-small");
     button.addEventListener("click", function () {
-        deleteRow(row.getAttribute("id"))
+        deleteRow(row.getAttribute("id"));
 })
 cell.append(button)
 clear();
 }
 
+// Llama a la función addRow() al presionar Enter
 document.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-        addRow(); // Llama a la función addRow() al presionar Enter
+        addRow(); 
     }
 });
 
 function clear() {
     document.getElementsByTagName('input')[0].value = ''
     document.getElementsByTagName('input')[1].value = ''
-    document.getElementsByTagName('input')[2].value = ''
-    
+    document.getElementsByTagName('input')[2].value = ''    
     document.getElementsByTagName('input')[0].focus()
 }
 
